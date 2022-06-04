a4d55879a746a9e875cadf25bcb3d86b78132e8a
runethorbek <runethorbek@gmail.com>
2022-06-04 10:02:49 +0200

    aligned with book

--- app/routers/order.py
+++ app/routers/order.py
@@ -10,5 +10,5 @@ router = APIRouter(tags=['income'])
 
 @router.post('/order', status_code=HTTPStatus.OK)
 async def order_call(order: str) -> Dict[str, str]:
-    logger.info(f'Incoming order: {order}')
-    return {'order': order}
+    logger.info(f'Incoming order received: {order}')
+    return {'Order Received': order}
